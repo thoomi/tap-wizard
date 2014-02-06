6fd692910eef777c7be387a3100438dab8231ed7
thoomi <thomi-blank@gmx.de>
2014-02-06 22:02:32 +0100

    Change app name; Added fs to app.js

--- app.js
+++ app.js
@@ -6,16 +6,22 @@ var app        = express();
 var server     = require('http').createServer(app);
 var io         = require('socket.io').listen(server);
 var gameserver = require('./game/gameserver.js');
-var path = require('path');
-
+var path 	   = require('path');
+var fs         = require('fs');
 
 app.set('port', process.env.PORT || 3000);
 
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(req, res) {
+/*app.get('/', function(req, res) {
     res.sendfile(__dirname + '/public/index.html');
+});*/
+
+app.get('/', function(req, res) {
+    fs.readFile(__dirname + '/public/index.html', 'utf8', function(err, text){
+        res.send(text);
+    });
 });
 
 server.listen(app.get('port'), function(){
