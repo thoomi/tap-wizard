18dd939e6c251e52a6d459b76b601c975a7c9e60
Thomas Blank <t.blank@zebresel.com>
2014-02-19 16:56:42 +0100

    Removed dummy data in playergame.js

--- client/app/scripts/controllers/playergame.js
+++ client/app/scripts/controllers/playergame.js
@@ -5,23 +5,6 @@ angular.module('clientApp')
 
     $scope.cards = [];
 
-    $scope.cards.push({suit: 'red', value: 13});
-    $scope.cards.push({suit: 'red', value: 13});
-    $scope.cards.push({suit: 'red', value: 13});
-    $scope.cards.push({suit: 'red', value: 13});
-    $scope.cards.push({suit: 'red', value: 13});
-    $scope.cards.push({suit: 'red', value: 13});
-    $scope.cards.push({suit: 'red', value: 13});
-    $scope.cards.push({suit: 'red', value: 13});
-    $scope.cards.push({suit: 'red', value: 13});
-    $scope.cards.push({suit: 'red', value: 13});
-    $scope.cards.push({suit: 'red', value: 13});
-    $scope.cards.push({suit: 'red', value: 13});
-    $scope.cards.push({suit: 'red', value: 13});
-    $scope.cards.push({suit: 'red', value: 13});
-    $scope.cards.push({suit: 'red', value: 13});
-    $scope.cards.push({suit: 'red', value: 13});
-
     $scope.currentRound = 0;
     $scope.notification = "Waiting for the host to start the game!";
     $scope.isGuessTricksDisabled = true;
