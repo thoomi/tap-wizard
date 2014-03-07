85b693cd550d1fc5b9a781048d977b4e660f6ce4
Thomas Blank <t.blank@zebresel.com>
2014-03-07 16:22:58 +0100

    Tried to reorganize code

--- app.js
+++ app.js
@@ -1,60 +1,73 @@
 /**
  * Module dependencies.
  */
-var express      = require('express');
-var app          = express();
-var server       = require('http').createServer(app);
-var io           = require('socket.io').listen(server);
-var sio          = require('session.socket.io');
-var gameserver   = require('./game/gameserver.js');
-var path         = require('path');
-var fs           = require('fs');
+var express    = require('express');
+var fs         = require('fs');
+var GameServer = require('./game/gameserver.js');
+var http       = require('http');
+var path       = require('path');
+var socketIo   = require('socket.io');
+var sSocketIo  = require('session.socket.io');
 
 
-var cookieParser = express.cookieParser('0123456789');
+/**
+ * Cookie parser and session store setup
+ */
+var cookieParser = express.cookieParser();
 var sessionStore = new express.session.MemoryStore();
 
 
-app.set('port', process.env.PORT || 3000);
+/**
+ * Express server, http server and socket.io setup
+ */
+var app    = express();
+var server = http.createServer(app);
+var io     = socketIo.listen(server);
+var sio    = new sSocketIo(io, sessionStore, cookieParser);
 
+
+/**
+ * Setup some express use values
+ */
+app.use(express.bodyParser());
+app.use(express.methodOverride());
 app.use(cookieParser);
-app.use(express.session({ secret: '0123456789', store: sessionStore }));
+app.use(express.session({ secret: 'tapWizard', store: sessionStore }));
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
+app.set('port', process.env.PORT || 3000);
 
 
-var sessionSockets = new sio(io, sessionStore, cookieParser);
-
-
-app.get('/', function(req, res) {
-    fs.readFile(__dirname + '/public/index.html', 'utf8', function(err, text){
-        res.send(text);
+/**
+ * Define which static files to serve in dev end prod mode
+ */
+app.configure('development', function() {
+    app.get('/', function(req, res) {
+        fs.readFile(__dirname + '/client/app/index.html', 'utf8', function(err, text){
+            res.send(text);
+        });
     });
 });
 
-// Development
-/*
-app.get('/', function(req, res) {
-    fs.readFile(__dirname + '/client/app/index.html', 'utf8', function(err, text){
-        res.send(text);
+app.configure('production', function() {
+    app.get('/', function(req, res) {
+        fs.readFile(__dirname + '/public/index.html', 'utf8', function(err, text){
+            res.send(text);
+        });
     });
-});*/
+});
 
 
+/**
+ * Listen to events on specified port
+ */
 server.listen(app.get('port'), function(){
     console.log('Express server listening on port ' + app.get('port'));
 });
 
 
-// Listen for Socket.IO Connections
-sessionSockets.on('connection', function (err, socket, session) {
-
-    if (err == null && session !== undefined) {
-        console.log('Session: ' +  session.id);
-        gameserver.initializeListeners(io, socket, sessionSockets);
-    }
-    else {
-        // Send error to client
-        socket.emit('error', err);
-    }
-});
+/**
+ * Initialize a game server
+ */
+var gameServer = new GameServer(sio);
+gameServer.setup();
--- game/gameserver.js
+++ game/gameserver.js
@@ -1,261 +1,303 @@
-var GameTable  = require('./gametable.js').GameTable;
-var Host       = require('./host.js').Host;
-var Player     = require('./player.js').Player;
-
-var io;
-var sessionSockets;
-
-var gameTables  = {};
+/**
+ * Module dependencies
+ */
 
 
-
-/* *******************************
-* *
-* HOST FUNCTIONS *
-* *
-******************************* */
 
 /**
-* The 'START GAME' button was clicked and 'hostCreateNewGame' event occurred.
-*/
-function hostCreateNewGame() {
-    var socket = this;
-    // Get session for the socket
-    sessionSockets.getSession(socket, function (err, session) {
+ * Module exports
+ */
+module.exports = GameServer;
+
+
+/**
+ * Constructor
+ */
+function GameServer(sio) {
+    this.sio = sio;
+}
+
+GameServer.prototype.setup = function() {
+    var gameServer = this;
+
+    gameServer.sio.on('connection', function (err, socket, session) {
+
         if (err === null && session !== undefined) {
-            // Create a new host
-            var host = new Host(session.id);
-            host.setSocket(socket);
-
-            // Create a unique Socket.IO Room / GameId
-            // TODO: Develop a proper way to generate a unique gameId
-            var newGameId = ( Math.random() * 100 ) | 0;
-            //var newGameId = '1';
-
-            // Create a new game table
-            gameTables[newGameId] = new GameTable(newGameId, host);
-
-            // Join the specific room for the game
-            socket.join(newGameId.toString());
-
-            // Save the gameId also to the session object
-            session.gameId = newGameId;
-            session.client = 'host';
-            session.save();
-
-            // Return the GameId to the browser client
-            socket.emit('newGameCreated', { gameId: newGameId });
+            console.log('Session: ' + session.id);
+            gameServer.initListeners(socket);
         }
         else {
-            socket.emit('sessionError', err);
+            // Send error to client
+            socket.emit('error', err);
         }
     });
-}
-
-/*
-* Enough players have joined! Start the game.
-* @param gameId The game ID / room ID
-*/
-function hostPrepareGame() {
-    var socket = this;
-    // Get session for the socket
-    sessionSockets.getSession(socket, function (err, session) {
-        if (err === null && session !== undefined && session.gameId !== undefined && session.client === 'host') {
-
-            var gameTable = gameTables[session.gameId];
-
-            if (gameTable !== undefined) {
-                var data = { maxRounds : gameTable.getNumberOfRounds(), currentRound : gameTable.getCurrentRound() };
-                io.sockets.in(session.gameId).emit('beginNewGame', data);
-                gameTable.prepareNewGame();
-            }
-        }
-        else {
-            socket.emit('sessionError', err);
-        }
-    });
-}
-
-function hostStartRound() {
-    var socket = this;
-    // Get session for the socket
-    sessionSockets.getSession(socket, function (err, session) {
-        if (err === null && session !== undefined && session.gameId !== undefined && session.client === 'host') {
-
-            var gameTable = gameTables[session.gameId];
-
-            if (gameTable !== undefined) {
-                io.sockets.in(session.gameId).emit('startNewRound', gameTable.getCurrentRound());
-                gameTable.dealCards();
-            }
-        }
-        else {
-            socket.emit('sessionError', err);
-        }
-    });
-}
+};
 
 
-/* *****************************
-* *
-* PLAYER FUNCTIONS *
-* *
-***************************** */
-
-/**
-* A player clicked the 'JOIN GAME' button.
-* Attempt to connect them to the room that matches
-* the gameId entered by the player.
-* @param data Contains data entered via player's input - playerName and gameId.
-*/
-function playerJoinGame(data) {
-    var socket = this;
-    // Get session for the socket
-    sessionSockets.getSession(socket, function (err, session) {
-        if (err === null && session !== undefined) {
-            // Look up the room ID in the Socket.IO manager object and the game table in the gameTables object
-            var room      = io.sockets.manager.rooms["/" + data.gameId];
-            var gameTable = gameTables[data.gameId];
-            // If the room and game exists
-            if ( room !== undefined && gameTable !== undefined ) {
-
-                // Create a new player
-                var player = new Player(session.id, data.playerName);
-                player.setSocket(socket);
-
-                // Join the room and the game
-                socket.join(data.gameId);
-                gameTable.addPlayer(player);
-
-                // Save the gameId and player object to the session
-                session.gameId = data.gameId;
-                session.client = 'player';
-                session.save();
-
-                // Add the session id to the data object
-                data.playerId      = session.id;
-                data.guessedTricks = 0;
-                data.points        = 0;
-
-                // Emit an event notifying the host that a player has joined the game
-                gameTable.host.emit('playerJoinedGame', data);
-
-                // Send a confirmation message to the client
-                socket.emit('playerJoinSuccess');
-            }
-            else {
-                // Otherwise, send an error message back to the player.
-                socket.emit('error', { message: "This room does not exist." });
-            }
-        }
-        else {
-            socket.emit('sessionError', err);
-        }
-    });
-}
-
-
-
-function playerThrowCard(data) {
-    var socket = this;
-    // Get session for the socket
-    sessionSockets.getSession(socket, function (err, session) {
-        if (err === null && session !== undefined && session.gameId !== undefined && session.client === 'player') {
-
-            var gameTable = gameTables[session.gameId];
-
-            if (gameTable !== undefined) {
-                if (gameTable.isCardAllowed(data.card, session.id)) {
-                    gameTable.playCard(data.card, session.id);
-                }
-                else {
-                    socket.emit('cardNotAllowed', data.card);
-                }
-            }
-        }
-        else {
-            socket.emit('sessionError', err);
-        }
-    });
-}
-
-
-function playerGuessTricks(data) {
-    var socket = this;
-    // Get session for the socket
-    sessionSockets.getSession(socket, function (err, session) {
-        if (err === null && session !== undefined && session.gameId !== undefined && session.client === 'player') {
-
-            var gameTable = gameTables[session.gameId];
-
-            if (gameTable !== undefined) {
-                gameTable.playerGuessedTricks(data.guessedTricks, session.id);
-            }
-        }
-        else {
-            socket.emit('sessionError', err);
-        }
-    });
-}
-
-/**
-* A player or the host disconnected
-*/
-function disconnect() {
-    var socket = this;
-    // Get session for the socket
-    sessionSockets.getSession(socket, function (err, session) {
-        if (err === null && session !== undefined && session.gameId !== undefined) {
-
-            // Get the game the player or host is in
-            var gameTable = gameTables[session.gameId];
-
-            if(gameTable !== undefined) {
-                if(gameTable.host.getId() === session.id) {
-                    io.sockets.in(gameTable.gameId).emit('hostDisconnected');
-                    gameTable.gameOver();
-                    // Delete game table
-                    gameTable = null;
-                }
-                else {
-                    if (gameTable.getState() === 'waiting') {
-                        gameTable.removePlayer(session.id);
-                        io.sockets.in(gameTable.gameId).emit('playerLeftGame', session.id);
-                    }
-                    else {
-                        // This is a very, very simple solution for the disconnect problem...
-                        gameTable.gameOver();
-                        gameTable = null;
-                    }
-
-                }
-            }
-        }
-        else {
-            socket.emit('sessionError', err);
-        }
-    });
-}
-
-
-
-/**
-* This function is called by app.js to initialize a new game instance.
-*/
-exports.initializeListeners = function(sio, socket, sSockets) {
-    io = sio;
-    sessionSockets = sSockets;
-
-    //socket.emit('connected', { message: "You are connected!" });
-
-    // Host Events
-    socket.on('hostCreateNewGame', hostCreateNewGame);
-    socket.on('hostPrepareGame', hostPrepareGame);
-    socket.on('hostStartRound', hostStartRound);
-
-    // Player Events
-    socket.on('playerJoinGame', playerJoinGame);
-    socket.on('playerThrowCard', playerThrowCard);
-    socket.on('playerGuessTricks', playerGuessTricks);
+GameServer.prototype.initListeners = function(socket) {
+    socket.on('createNewGame', createNewGame);
+    socket.on('startGame', startGame);
+    socket.on('joinGame', joinGame);
     socket.on('disconnect', disconnect);
 };
+
+// var GameTable  = require('./gametable.js').GameTable;
+// var Host       = require('./host.js').Host;
+// var Player     = require('./player.js').Player;
+
+// var io;
+// var sessionSockets;
+
+// var gameTables  = {};
+
+
+// /* *******************************
+// * *
+// * HOST FUNCTIONS *
+// * *
+// ******************************* */
+
+// /**
+// * The 'START GAME' button was clicked and 'hostCreateNewGame' event occurred.
+// */
+// function hostCreateNewGame() {
+//     var socket = this;
+//     // Get session for the socket
+//     sessionSockets.getSession(socket, function (err, session) {
+//         if (err === null && session !== undefined) {
+//             // Create a new host
+//             var host = new Host(session.id);
+//             host.setSocket(socket);
+
+//             // Create a unique Socket.IO Room / GameId
+//             // TODO: Develop a proper way to generate a unique gameId
+//             var newGameId = ( Math.random() * 100 ) | 0;
+//             //var newGameId = '1';
+
+//             // Create a new game table
+//             gameTables[newGameId] = new GameTable(newGameId, host);
+
+//             // Join the specific room for the game
+//             socket.join(newGameId.toString());
+
+//             // Save the gameId also to the session object
+//             session.gameId = newGameId;
+//             session.client = 'host';
+//             session.save();
+
+//             // Return the GameId to the browser client
+//             socket.emit('newGameCreated', { gameId: newGameId });
+//         }
+//         else {
+//             socket.emit('sessionError', err);
+//         }
+//     });
+// }
+
+// /*
+// * Enough players have joined! Start the game.
+// * @param gameId The game ID / room ID
+// */
+// function hostPrepareGame() {
+//     var socket = this;
+//     // Get session for the socket
+//     sessionSockets.getSession(socket, function (err, session) {
+//         if (err === null && session !== undefined && session.gameId !== undefined && session.client === 'host') {
+
+//             var gameTable = gameTables[session.gameId];
+
+//             if (gameTable !== undefined) {
+//                 var data = { maxRounds : gameTable.getNumberOfRounds(), currentRound : gameTable.getCurrentRound() };
+//                 io.sockets.in(session.gameId).emit('beginNewGame', data);
+//                 gameTable.prepareNewGame();
+//             }
+//         }
+//         else {
+//             socket.emit('sessionError', err);
+//         }
+//     });
+// }
+
+// function hostStartRound() {
+//     var socket = this;
+//     // Get session for the socket
+//     sessionSockets.getSession(socket, function (err, session) {
+//         if (err === null && session !== undefined && session.gameId !== undefined && session.client === 'host') {
+
+//             var gameTable = gameTables[session.gameId];
+
+//             if (gameTable !== undefined) {
+//                 io.sockets.in(session.gameId).emit('startNewRound', gameTable.getCurrentRound());
+//                 gameTable.dealCards();
+//             }
+//         }
+//         else {
+//             socket.emit('sessionError', err);
+//         }
+//     });
+// }
+
+
+// /* *****************************
+// * *
+// * PLAYER FUNCTIONS *
+// * *
+// ***************************** */
+
+// /**
+// * A player clicked the 'JOIN GAME' button.
+// * Attempt to connect them to the room that matches
+// * the gameId entered by the player.
+// * @param data Contains data entered via player's input - playerName and gameId.
+// */
+// function playerJoinGame(data) {
+//     var socket = this;
+//     // Get session for the socket
+//     sessionSockets.getSession(socket, function (err, session) {
+//         if (err === null && session !== undefined) {
+//             // Look up the room ID in the Socket.IO manager object and the game table in the gameTables object
+//             var room      = io.sockets.manager.rooms["/" + data.gameId];
+//             var gameTable = gameTables[data.gameId];
+//             // If the room and game exists
+//             if ( room !== undefined && gameTable !== undefined ) {
+
+//                 // Create a new player
+//                 var player = new Player(session.id, data.playerName);
+//                 player.setSocket(socket);
+
+//                 // Join the room and the game
+//                 socket.join(data.gameId);
+//                 gameTable.addPlayer(player);
+
+//                 // Save the gameId and player object to the session
+//                 session.gameId = data.gameId;
+//                 session.client = 'player';
+//                 session.save();
+
+//                 // Add the session id to the data object
+//                 data.playerId      = session.id;
+//                 data.guessedTricks = 0;
+//                 data.points        = 0;
+
+//                 // Emit an event notifying the host that a player has joined the game
+//                 gameTable.host.emit('playerJoinedGame', data);
+
+//                 // Send a confirmation message to the client
+//                 socket.emit('playerJoinSuccess');
+//             }
+//             else {
+//                 // Otherwise, send an error message back to the player.
+//                 socket.emit('error', { message: "This room does not exist." });
+//             }
+//         }
+//         else {
+//             socket.emit('sessionError', err);
+//         }
+//     });
+// }
+
+
+
+// function playerThrowCard(data) {
+//     var socket = this;
+//     // Get session for the socket
+//     sessionSockets.getSession(socket, function (err, session) {
+//         if (err === null && session !== undefined && session.gameId !== undefined && session.client === 'player') {
+
+//             var gameTable = gameTables[session.gameId];
+
+//             if (gameTable !== undefined) {
+//                 if (gameTable.isCardAllowed(data.card, session.id)) {
+//                     gameTable.playCard(data.card, session.id);
+//                 }
+//                 else {
+//                     socket.emit('cardNotAllowed', data.card);
+//                 }
+//             }
+//         }
+//         else {
+//             socket.emit('sessionError', err);
+//         }
+//     });
+// }
+
+
+// function playerGuessTricks(data) {
+//     var socket = this;
+//     // Get session for the socket
+//     sessionSockets.getSession(socket, function (err, session) {
+//         if (err === null && session !== undefined && session.gameId !== undefined && session.client === 'player') {
+
+//             var gameTable = gameTables[session.gameId];
+
+//             if (gameTable !== undefined) {
+//                 gameTable.playerGuessedTricks(data.guessedTricks, session.id);
+//             }
+//         }
+//         else {
+//             socket.emit('sessionError', err);
+//         }
+//     });
+// }
+
+// /**
+// * A player or the host disconnected
+// */
+// function disconnect() {
+//     var socket = this;
+//     // Get session for the socket
+//     sessionSockets.getSession(socket, function (err, session) {
+//         if (err === null && session !== undefined && session.gameId !== undefined) {
+
+//             // Get the game the player or host is in
+//             var gameTable = gameTables[session.gameId];
+
+//             if(gameTable !== undefined) {
+//                 if(gameTable.host.getId() === session.id) {
+//                     io.sockets.in(gameTable.gameId).emit('hostDisconnected');
+//                     gameTable.gameOver();
+//                     // Delete game table
+//                     gameTable = null;
+//                 }
+//                 else {
+//                     if (gameTable.getState() === 'waiting') {
+//                         gameTable.removePlayer(session.id);
+//                         io.sockets.in(gameTable.gameId).emit('playerLeftGame', session.id);
+//                     }
+//                     else {
+//                         // This is a very, very simple solution for the disconnect problem...
+//                         gameTable.gameOver();
+//                         gameTable = null;
+//                     }
+
+//                 }
+//             }
+//         }
+//         else {
+//             socket.emit('sessionError', err);
+//         }
+//     });
+// }
+
+
+
+// /**
+// * This function is called by app.js to initialize a new game instance.
+// */
+// exports.initializeListeners = function(sio, socket, sSockets) {
+//     io = sio;
+//     sessionSockets = sSockets;
+
+//     //socket.emit('connected', { message: "You are connected!" });
+
+//     // Host Events
+//     socket.on('hostCreateNewGame', hostCreateNewGame);
+//     socket.on('hostPrepareGame', hostPrepareGame);
+//     socket.on('hostStartRound', hostStartRound);
+
+//     // Player Events
+//     socket.on('playerJoinGame', playerJoinGame);
+//     socket.on('playerThrowCard', playerThrowCard);
+//     socket.on('playerGuessTricks', playerGuessTricks);
+//     socket.on('disconnect', disconnect);
+// };
